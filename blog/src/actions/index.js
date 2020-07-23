576185192502c412ddd0bdd58f31db644a4a23f9
wein98 <wein9298@hotmail.com>
2020-07-23 15:59:57 +0800

    Async Actions with Redux Thunk

---
+++ blog/src/actions/index.js
@@ -0,0 +1,8 @@
+import jsonPlaceholder from '../apis/jsonPlaceHolder';
+
+// action creator that uses redux-thunk wired up in root index.js
+export const fetchPosts = async () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response})
+}
