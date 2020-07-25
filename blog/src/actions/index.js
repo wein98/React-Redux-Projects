4646dcadd26e67f6d51a4c875884947be5e2330e
wein98 <wein9298@hotmail.com>
2020-07-25 10:25:58 +0800

    Redux Store Design halfway

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,8 +1,8 @@
 import jsonPlaceholder from '../apis/jsonPlaceHolder';
 
 // action creator that uses redux-thunk wired up in root index.js
-export const fetchPosts = async () => async dispatch => {
+export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response})
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
