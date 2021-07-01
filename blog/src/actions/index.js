db1c0bb7bf2ee1911ed364814779c77575ea9a23
wein98 <wein9298@hotmail.com>
2021-07-01 17:39:06 +0800

    FINISHED blog. Section 19: Redux Store Design

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,23 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceHolder';
+
+//  overfetching solution 2
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    // this will automatically update the redux store and dispatch to all the reducers
+    await dispatch(fetchPosts());
+    // console.log("fetched posts")
+
+    // go through all the posts and just pull of 'userId' of each post and store all the unique ids
+    const userIds = _.uniq(_.map(getState().posts, 'userId'))
+    userIds.forEach(id => dispatch(fetchUser(id)))
+
+    // _.chain compact code
+    // _.chain(getState().posts)
+    //     .map('userId')
+    //     .uniq()
+    //     .forEach(id => dispatch(fetchUser(id)))
+    //     .value(); // execute
+}
 
 // action creator that uses redux-thunk wired up in root index.js
 export const fetchPosts = () => async dispatch => {
@@ -6,3 +25,18 @@ export const fetchPosts = () => async dispatch => {
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data })
+}
+
+// using lodash for overfetching solution 1
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ type: "FETCH_USER", payload: response.data })
+// })
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
+
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostsAndUsers();
     }
 
     renderList() {
@@ -17,6 +19,7 @@ class PostList extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
             )
@@ -34,4 +37,4 @@ const mapStateToProps = state => {
     return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPostsAndUsers })(PostList);
